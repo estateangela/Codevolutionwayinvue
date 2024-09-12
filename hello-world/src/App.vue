6bf19eb9f3802c8b6917cdb9a2c5730ae7807417
e.an <161005688+estateangela@users.noreply.github.com>
2024-09-12 17:46:44 +0800

    add

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,18 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div>{{ greet }} {{ name }}</div>
+    <div v-html="channel"></div>
+    <div v-html="hack"></div>
+    <h2 v-bind:id="heading">Heading</h2>
+    <button v-bind:disabled="isDisabled">Click me</button>
+    <h2 class="underline">underlined text</h2>
+    <h2 v-bind:class="status">Status</h2>
+    <h2 v-bind:class="isPromoted && 'promoted'">Promoted movie</h2>
+    <h2 v-bind:class="isSoldOut ? 'sold-out' : 'new'">Soldout? movie</h2>
+    <h2 v-bind:class="['new', 'promoted']">Newly promoted movie</h2>
+    <h2 v-bind:class="[isPromoted && 'promoted', isSoldOut ? 'sold-out' : 'new']">Array conditional movie</h2>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      greet: 'Hello',
+      name: "Vishwas",
+      channel: "<b>Codevolution</b>",
+      hack: `<a href="#" onclick="alert('you have been hacked')">Win a prize</a>`,
+      headingId: 'heading',
+      isDisabled: false,
+      status: 'success',
+      isPromoted: false,
+      isSoldOut: false,
+    };
   }
+
 }
 </script>
 
@@ -24,5 +44,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.underline {
+  text-decoration: underline;
+}
+
+.promoted  {
+  font-style: italic;
+}
+
+.new {
+  color: olivedrab;
+}
+
+.sold-out {
+  color: red;
 }
 </style>
