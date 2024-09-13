05d137c2ec20ea99a78e74216d4e93f3f9fd6d96
e.an <161005688+estateangela@users.noreply.github.com>
2024-09-13 13:09:34 +0800

    learning step by step

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -3,14 +3,23 @@
     <div>{{ greet }} {{ name }}</div>
     <div v-html="channel"></div>
     <div v-html="hack"></div>
-    <h2 v-bind:id="heading">Heading</h2>
-    <button v-bind:disabled="isDisabled">Click me</button>
+    <h2  :id="heading">Heading</h2>
+    <button  :disabled="isDisabled">Click me</button>
     <h2 class="underline">underlined text</h2>
-    <h2 v-bind:class="status">Status</h2>
-    <h2 v-bind:class="isPromoted && 'promoted'">Promoted movie</h2>
-    <h2 v-bind:class="isSoldOut ? 'sold-out' : 'new'">Soldout? movie</h2>
-    <h2 v-bind:class="['new', 'promoted']">Newly promoted movie</h2>
-    <h2 v-bind:class="[isPromoted && 'promoted', isSoldOut ? 'sold-out' : 'new']">Array conditional movie</h2>
+    <h2  :class="status">Status</h2>
+    <h2  :class="isPromoted && 'promoted'">Promoted movie</h2>
+    <h2  :class="isSoldOut ? 'sold-out' : 'new'">Soldout? movie</h2>
+    <h2  :class="['new', 'promoted']">Newly promoted movie</h2>
+    <h2  :class="[isPromoted && 'promoted', isSoldOut ? 'sold-out' : 'new']">Array conditional movie</h2>
+    <h2  :style="{
+      color: highlightColor,
+      'font-size': headersize + 'px',
+      padding : '20px',
+      }">Inline style</h2>
+      <h2  :style="headStyleObject">style object</h2>
+  
+  <div  :style="[baseStyleObject, successStyleObject]">success style</div>
+  <div  :style="[baseStyleObject, dangerStyleObject]">danger style</div>
   </div>
 </template>
 
@@ -30,8 +39,30 @@ export default {
       status: 'success',
       isPromoted: false,
       isSoldOut: false,
-    };
-  }
+      highlightColor: 'orange',
+      headersize: '50',
+      headStyleObject: {
+        color: 'orange',
+        fontSize: '50px',
+        padding: '20px',
+      },
+      baseStyleObject:{
+        fontSize: '50px',
+        padding: '10px',
+      },
+      successStyleObject: {
+        color: 'blueviolet',
+        backgroundColor: 'lightblue',
+        border: '1px solid black',
+
+      },
+      dangerStyleObject: {
+        color:'red',
+        backgroundColor: 'lightcoral',
+        border: '1px solid black',
+      },
+    }
+  },
 
 }
 </script>
